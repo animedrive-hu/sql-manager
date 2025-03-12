840424a03ec251d334e4b6a7472bff1c7c519a69
BlackFireHUN <33300671+BlackFireHUN@users.noreply.github.com>
2025-03-12 15:30:24 +0100

    Making it up to date/ modernise, adding sqlite, and improving security with prepared statements

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-const mysql = require('mysql');
-var con;
-var connection;
-module.exports = class sqlman {
-    constructor(data){
-        try {
-            connection = mysql.createConnection(data);
-            con = connection;
-        } catch(e) {
-            if(e) throw new Error("Something went wrong! ERR: \N"+e);
+const mysql = require('mysql2/promise');
+const sqlite3 = require('sqlite3').verbose();
+
+class Database {
+    constructor(config) {
+        this.config = config;
+        this.db = null;
+    }
+
+    async connect() {
+        if (this.config.type === 'mysql') {
+            this.db = await mysql.createPool({
+                host: this.config.host,
+                user: this.config.user,
+                password: this.config.password,
+                database: this.config.database,
+                waitForConnections: true,
+                connectionLimit: 10,
+                queueLimit: 0
+            });
+        } else if (this.config.type === 'sqlite') {
+            this.db = new sqlite3.Database(this.config.database, (err) => {
+                if (err) console.error('SQLite connection error:', err.message);
+            });
+        } else {
+            throw new Error('Unsupported database type');
         }
     }
 
-    async run(query, callback){
-        con.connect(function(e) {
-            if (e) throw new Error("Something went wrong! ERR: \N"+e);
-        });
-        con.query(`${query}`, function (error, results, fields) {
-            if (error) throw error;
-            return callback(results);
-        });
-        con.end();
+    async query(sql, params = []) {
+        if (this.config.type === 'mysql') {
+            const [rows] = await this.db.execute(sql, params);
+            return rows;
+        } else if (this.config.type === 'sqlite') {
+            return new Promise((resolve, reject) => {
+                this.db.all(sql, params, (err, rows) => {
+                    if (err) reject(err);
+                    else resolve(rows);
+                });
+            });
+        }
+    }
+
+    async close() {
+        if (this.config.type === 'mysql') {
+            await this.db.end();
+        } else if (this.config.type === 'sqlite') {
+            this.db.close((err) => {
+                if (err) console.error('Error closing SQLite database:', err.message);
+            });
+        }
     }
 }
+
+module.exports = Database;
